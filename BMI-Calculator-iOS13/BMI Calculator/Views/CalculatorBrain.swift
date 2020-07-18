3feee4c734b2fad13456107418dec17511c324c7
Pranav Srinivasan <58053456+pranavs2001@users.noreply.github.com>
2020-07-18 14:34:58 -0700

    Add files via upload
    Add BMI Calculator

---
+++ BMI-Calculator-iOS13/BMI Calculator/Views/CalculatorBrain.swift
@@ -0,0 +1,43 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Pranav Srinivasan on 6/19/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain{
+    
+    var bmi: BMI?
+    
+    mutating func calculateBMI(height: Float, weight: Float){
+        let bmiValue = ((weight) / pow(height, 2))
+        
+        if(bmiValue < 18.5){
+            bmi = BMI(value: bmiValue, advice: "Eat more food!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+        }
+        else if(bmiValue < 24.9){
+            bmi = BMI(value: bmiValue, advice: "You're in shape!", color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))
+        }
+        else{
+            bmi = BMI(value: bmiValue, advice: "Eat less junk!", color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1))
+        }
+        
+    }
+    
+    func getBMIValue() -> String{
+        let bmito1DecimalPlace = String(format: "%0.1f", bmi?.value ?? "0.0")
+        
+        return bmito1DecimalPlace
+    }
+    
+    func getAdvice() -> String{
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() ->UIColor{
+        return bmi?.color ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+}
