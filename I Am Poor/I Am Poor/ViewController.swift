ca57bd415b457802ee5b34388212fa51fcf90d88
Pranav Srinivasan <58053456+pranavs2001@users.noreply.github.com>
2020-07-18 14:38:55 -0700

    Add files via upload
    Add I Am Poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Pranav Srinivasan on 6/15/20.
+//  Copyright © 2020 Pranav Srinivasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
